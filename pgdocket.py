47a9ac81f948fdebfb782d04d9730f40a8f67c23
Aysha Khan <ayshabkhan@gmail.com>
2016-05-11 01:14:55 -0400

    final updates

--- pgdocket.py
+++ pgdocket.py
@@ -1,22 +1,15 @@
-#from selenium import webdriver
-
-#webdriver.get("webdriver.chrome.driver", "C:\\michellechavez\\Desktop\\chromedriver.exe"); 
-#driver = '/Users/michellechavez/Desktop/chromedriver'
-#driver.get("http://casesearch.courts.state.md.us/casesearch/");
-
-#driver = '/Users/michellechavez/Desktop/chromedriver'
-#browser = webdriver.Chrome(executable_path = path_to_chromedriver)
-#url = 'http://casesearch.courts.state.md.us/casesearch/'
-#browser.get(url)
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+driver = webdriver.Firefox()
+driver.get("http://casesearch.courts.state.md.us/casesearch/")
+checkbox = driver.find_element_by_name('disclaimer').click()
+button = driver.find_element_by_name('action')
+button.click()
+
+el = driver.find_element_by_name('locationCode')
+opt = [option for option in el.find_elements_by_tag_name('option') if option.text == "Prince George's County Circuit Court"][0]
+opt.click()
+driver.find_element_by_name("caseId").send_keys("CT151304X") # that string is a placeholder ID
+# for version 2.0: loop through case IDs (column 1) from data file scraped from docket (data.tsv)
+driver.find_elements_by_name('action')[2].click()
